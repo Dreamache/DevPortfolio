b70eb0ba35f2b2ca66ada4ab05936fc1c872f20c
Joao Pedro <aztrosistxd@gmail.com>
2023-09-11 02:14:32 -0300

    commit para resolver bug

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,37 +1,37 @@
 export default [
    {
       title: 'Python to Clojure Translator',
-      imgUrl: '/assets/tra.jpg',
+      imgUrl: '/public/assets/tra.jpg',
       stack: ['Python'],
       link: 'https://github.com/Dreamache/Python-to-Clojure-Translator',
    },
    {
       title: 'Developer Portfolio',
-      imgUrl: '/assets/dev.jpg',
+      imgUrl: '/public/assets/dev.jpg',
       stack: ['React', 'Tailwind', 'Vite'],
       link: 'https://github.com/Dreamache',
    },
    {
       title: 'Banco de dados Lab',
-      imgUrl: '/assets/SQL.jpg',
+      imgUrl: '/public/assets/SQL.jpg',
       stack: ['MySQL'],
       link: 'https://github.com/Dreamache/Banco-de-dados-Lab',
    },
    {
       title: 'PurplePass',
-      imgUrl: '/assets/purplepass.jpg',
+      imgUrl: '/public/assets/purplepass.jpg',
       stack: ['Javascript', 'HTML', 'CSS'],
       link: 'https://github.com/Dreamache/PurplePass',
    },
    {
       title: 'Astromovies',
-      imgUrl: '/assets/lafr.jpg',
+      imgUrl: '/public/assets/lafr.jpg',
       stack: ['React', 'Firebase', 'Vite'],
       link: 'https://github.com/Dreamache/Astromovies',
    },
    {
       title: 'Tasker Tracker',
-      imgUrl: '/assets/tasker222.jpg',
+      imgUrl: '/public/assets/tasker222.jpg',
       stack: ['Javascript', 'HTML', 'CSS'],
       link: 'https://github.com/Dreamache/Tasker-Tracker',
    },
